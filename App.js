c4b0963ee1347c78498f752b4ba7f41247313035
gmemmy <gmemmy22@gmail.com>
2019-11-07 00:04:32 +0100

    implement push notification for the app

--- App.js
+++ App.js
@@ -1,13 +1,75 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import { AppState } from 'react-native';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 import AppContainer from './src';
 import FlashMessage from "react-native-flash-message";
 
-export default function App() {
-  return (
-    <Fragment>
-      <AppContainer />
-      <FlashMessage position='bottom' />
-    </Fragment>
-  );
-}
+class App extends Component {
+  registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+      );
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(
+          Permissions.NOTIFICATIONS
+        );
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      let token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  };
+  componentDidMount() {
+    this.registerForPushNotificationsAsync();
+    AppState.addEventListener('change', this.handleAppStateChange);
+  }
 
+  _handleNotification = notification => {
+    this.setState({ notification: notification });
+  };
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  }
+  sendPushNotification = async () => {
+    const message = {
+      to: Notifications.getExpoPushTokenAsync(),
+      sound: 'default',
+      title: 'Crypto App',
+      body: 'Hey there! Crypto App',
+      date: new Date(Date.now())
+    };
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  };
+  handleAppStateChange = (appState) => {
+    if (appState === 'background') {
+      setTimeout(this.sendPushNotification(), 300000)
+    }
+  }
+  render() {
+    return (
+      <Fragment>
+        <AppContainer />
+        <FlashMessage position='bottom' />
+      </Fragment>
+    );
+  }
+}
+export default App;
